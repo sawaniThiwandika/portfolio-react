e1a0160adaf4700529c6e41f3192802d9b7d18dc
Sawani Thiwandika <thiwandika.whs02@gmail.com>
2025-03-15 16:53:43 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+      <>
+        <button
+            className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
+          Click Me
+        </button>
+      </>
+  )
+}
+
+export default App
