142bac01adea54be58405b46f7085237000bf963
Sawani Thiwandika <thiwandika.whs02@gmail.com>
2025-03-16 16:00:16 +0530

    set portfolio component to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 
 import './App.css'
+import {Portfolio} from "./components/Portfolio.tsx";
 
 function App() {
 
 
   return (
       <>
-        <button
-            className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
-          Click Me
-        </button>
+          <Portfolio/>
       </>
   )
 }
