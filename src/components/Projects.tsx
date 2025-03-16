24fef51663eed6d3bbeaf31f339c37c93fe1911d
Sawani Thiwandika <thiwandika.whs02@gmail.com>
2025-03-16 16:02:25 +0530

    create project component

---
+++ src/components/Projects.tsx
@@ -0,0 +1,12 @@
+export function Projects() {
+    return (
+        <section id="projects" className="p-6">
+            <h2 className="text-3xl font-semibold text-purple-400">Projects</h2>
+            <ul className="mt-4">
+                <li><a href="https://github.com/sawaniThiwandika/green-shadow-frontend-with-react" className="text-blue-300">GreenShadow - Farm Management System</a></li>
+                <li><a href="https://github.com/sawaniThiwandika/pos-system-with-html" className="text-blue-300">POS System</a></li>
+                <li><a href="https://github.com/sawaniThiwandika/green-shadow-frontend-with-react" className="text-blue-300">MyDiary - Notes App</a></li>
+            </ul>
+        </section>
+    );
+}
