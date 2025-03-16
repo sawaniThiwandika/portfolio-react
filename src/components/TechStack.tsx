934bc6a664e99773859d8e3d8ce5bf26fe487943
Sawani Thiwandika <thiwandika.whs02@gmail.com>
2025-03-16 16:03:01 +0530

    create techStack component

---
+++ src/components/TechStack.tsx
@@ -0,0 +1,15 @@
+export function TechStack() {
+    return (
+        <section id="tech" className="p-6">
+            <h2 className="text-3xl font-semibold text-pink-400">Tech Stack</h2>
+            <ul className="mt-4 flex space-x-6">
+                <li>React</li>
+                <li>Node.js</li>
+                <li>TypeScript</li>
+                <li>Express.js</li>
+                <li>Prisma</li>
+                <li>MySQL</li>
+            </ul>
+        </section>
+    );
+}
