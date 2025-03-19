92aaef2bf29cb9a6187792c9af08a0aac2314fda
sawaniThiwandika <thiwandika.whs02@gmail.com>
2025-03-19 23:01:13 +0530

    modify the about me section with colors

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,15 +1,20 @@
 export function AboutMe() {
     return (
-        <section id="about" className="mt-16 p-6 w-5/6 flex flex-col md:flex-row items-center justify-between">
+        <section id="about" className="mt-16 p-10 w-5/6 flex flex-col md:flex-row items-center justify-between">
             <div className="w-full md:w-1/5 text-center p-4 flex items-center justify-center">
-                <h1 className="text-5xl font-medium">About Me</h1>
+                <h1 className="text-5xl font-semibold text-pink-800">About Me</h1>
             </div>
-            <div className="w-px bg-gray-400 h-20 hidden md:block"></div>
-            <div className="w-full md:w-4/5 pl-4 md:flex items-center">
-                <p className="mt-4 md:mt-0">I am a Software Engineer from Sri Lanka, currently studying at IJSE & the University
-                    of Moratuwa. Passionate about full-stack development, I specialize in building efficient, scalable,
-                    and user-friendly applications. With experience in React, Node.js, and database management, I strive to
-                    create innovative solutions that enhance user experiences.</p>
+            <div className="w-px bg-gray-400 h-24 hidden md:block"></div>
+            <div className="w-full md:w-4/5 pl-6 md:flex items-center">
+                <p className="mt-4 md:mt-0 text-lg  leading-relaxed">
+                    I am a Software Engineer from Sri Lanka, currently studying at IJSE & the University of Moratuwa.
+                    Passionate about full-stack development, I specialize in building efficient, scalable, and user-friendly
+                    applications. With experience in React, Node.js, and database management, I strive to create innovative
+                    solutions that enhance user experiences.I am a Software Engineer from Sri Lanka, currently studying at IJSE & the University of Moratuwa.
+                    Passionate about full-stack development, I specialize in building efficient, scalable, and user-friendly
+                    applications. With experience in React, Node.js, and database management, I strive to create innovative
+                    solutions that enhance user experiences.
+                </p>
             </div>
         </section>
     );
