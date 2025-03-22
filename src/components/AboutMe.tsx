c309f7376c0caacc6f5aca53f0c4ce06c2fb34d5
sawaniThiwandika <thiwandika.whs02@gmail.com>
2025-03-22 22:04:32 +0530

    modify the about me section - change content in paragraph

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -11,11 +11,15 @@ export function AboutMe() {
 
 
             <div className="w-full md:w-4/5 pl-6 md:flex items-center">
-                <p className="mt-4 md:mt-0 text-lg leading-relaxed text-gray-400 pr-6" style={{ textAlign: "justify" }}>
-                    I am a Software Engineer from Sri Lanka, currently studying at <strong>IJSE</strong> & the <strong>University of Moratuwa</strong>.
-                    Passionate about full-stack development, I specialize in building efficient, scalable, and user-friendly applications.
-                    With experience in <strong>React</strong>, <strong>Node.js</strong>, and <strong>database management</strong>,
-                    I strive to create innovative solutions that enhance user experiences.
+                <p className="mt-4 md:mt-0 text-lg leading-relaxed text-gray-400 pr-6" style={{textAlign: "justify"}}>
+                    I am a dedicated Software Engineer from Sri Lanka, currently pursuing a BSc (Hons) in IT at the
+                    University of Moratuwa and a GDSE diploma at IJSE. Passionate about full-stack development,
+                    I specialize in designing and building scalable, efficient, and user-friendly applications.
+
+                    With a strong problem-solving mindset and keen attention to detail, I excel in stress management,
+                    allowing me to remain focused and productive in high-pressure environments. Committed to continuous
+                    learning and professional growth, I actively seek opportunities to improve my skills and contribute
+                    to impactful projects.
                 </p>
             </div>
         </section>
