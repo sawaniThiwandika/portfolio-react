6d7e35c45ab5323cffbb6f9be3d558fbdae6a9d5
sawaniThiwandika <thiwandika.whs02@gmail.com>
2025-03-23 16:17:45 +0530

    align headers to center in each div

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -2,11 +2,12 @@ import { SiReact, SiReactrouter, SiTailwindcss, SiBootstrap, SiHtml5, SiCss3, Si
     SiTypescript, SiSpringboot, SiPython, SiNodedotjs, SiFirebase, SiMysql, SiMongodb,
     SiFigma, SiPostman, SiC, SiExpress } from "react-icons/si";
 import { FaJava } from "react-icons/fa";
+
 interface TechStackProps {
     darkMode: boolean;
-
 }
-export function TechStack({darkMode}:TechStackProps ) {
+
+export function TechStack({darkMode}: TechStackProps) {
     return (
         <section id="tech" className="mt-20 py-16 px-6 w-5/6">
             <h2 className={`text-4xl font-bold ${darkMode ? 'text-purple-400' : 'text-purple-600'} text-center mb-12`}>
@@ -14,8 +15,8 @@ export function TechStack({darkMode}:TechStackProps ) {
             </h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
 
-                <div className={` p-6 rounded-2xl shadow-lg border ${darkMode ? 'border-gray-600' : 'border-gray-300'} hover:scale-105 transition-transform duration-300`}>
-                    <h3  className={` text-2xl font-semibold ${darkMode ? 'text-purple-300' : 'text-purple-700'} flex items-center space-x-2`}>
+                <div className={`text-center p-6 rounded-2xl shadow-lg border ${darkMode ? 'border-gray-600' : 'border-gray-300'} hover:scale-105 transition-transform duration-300`}>
+                    <h3 className={`text-2xl font-semibold ${darkMode ? 'text-purple-300' : 'text-purple-700'} text-center flex justify-center space-x-2`}>
                         <span>Languages</span>
                     </h3>
                     <ul className={`mt-4 ${darkMode ? 'text-gray-400' : 'text-gray-700'} font-medium space-y-3`}>
@@ -28,7 +29,7 @@ export function TechStack({darkMode}:TechStackProps ) {
                 </div>
 
                 <div className={`p-6 rounded-2xl shadow-lg border ${darkMode ? 'border-gray-600' : 'border-gray-300'} hover:scale-105 transition-transform duration-300`}>
-                    <h3 className={`text-2xl font-semibold ${darkMode ? 'text-purple-300' : 'text-purple-700'} flex items-center space-x-2`}>
+                    <h3 className={`text-2xl font-semibold ${darkMode ? 'text-purple-300' : 'text-purple-700'} text-center flex justify-center space-x-2`}>
                         <span>Frontend</span>
                     </h3>
                     <ul className={`mt-4 ${darkMode ? 'text-gray-400' : 'text-gray-700'} font-medium space-y-3`}>
@@ -42,7 +43,7 @@ export function TechStack({darkMode}:TechStackProps ) {
                 </div>
 
                 <div className={`p-6 rounded-2xl shadow-lg border ${darkMode ? 'border-gray-600' : 'border-gray-300'} hover:scale-105 transition-transform duration-300`}>
-                    <h3 className={`text-2xl font-semibold ${darkMode ? 'text-purple-300' : 'text-purple-700'} flex items-center space-x-2`}>
+                    <h3 className={`text-2xl font-semibold ${darkMode ? 'text-purple-300' : 'text-purple-700'} text-center flex justify-center space-x-2`}>
                         <span>Backend</span>
                     </h3>
                     <ul className={`mt-4 ${darkMode ? 'text-gray-400' : 'text-gray-700'} font-medium space-y-3`}>
@@ -55,7 +56,7 @@ export function TechStack({darkMode}:TechStackProps ) {
                 </div>
 
                 <div className={`p-6 rounded-2xl shadow-lg border ${darkMode ? 'border-gray-600' : 'border-gray-300'} hover:scale-105 transition-transform duration-300`}>
-                    <h3 className={`text-2xl font-semibold ${darkMode ? 'text-purple-300' : 'text-purple-700'} flex items-center space-x-2`}>
+                    <h3 className={`text-2xl font-semibold ${darkMode ? 'text-purple-300' : 'text-purple-700'} text-center flex justify-center space-x-2`}>
                         <span>Database</span>
                     </h3>
                     <ul className={`mt-4 ${darkMode ? 'text-gray-400' : 'text-gray-700'} font-medium space-y-3`}>
@@ -65,7 +66,7 @@ export function TechStack({darkMode}:TechStackProps ) {
                 </div>
 
                 <div className={`p-6 rounded-2xl shadow-lg border ${darkMode ? 'border-gray-600' : 'border-gray-300'} hover:scale-105 transition-transform duration-300`}>
-                    <h3 className={`text-2xl font-semibold ${darkMode ? 'text-purple-300' : 'text-purple-700'} flex items-center space-x-2`}>
+                    <h3 className={`text-2xl font-semibold ${darkMode ? 'text-purple-300' : 'text-purple-700'} text-center flex justify-center space-x-2`}>
                         <span>Other</span>
                     </h3>
                     <ul className={`mt-4 ${darkMode ? 'text-gray-400' : 'text-gray-700'} font-medium space-y-3`}>
