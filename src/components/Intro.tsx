492b7c17e6101315d4a6aeec24215279d967bbc2
sawaniThiwandika <thiwandika.whs02@gmail.com>
2025-04-01 13:11:16 +0530

    modify- intro page responsiveness

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,22 +1,18 @@
-
 import cover from "../assets/cover/cropCoverImage.png";
-
-
 import '../styles/IntroCss.css';
 
 export function Intro() {
 
-
     return (
-        <section className="text-center mt-10 flex flex-col md:flex-row items-center justify-between">
+        <section className="text-center mt-24 flex flex-col md:flex-row items-center justify-between">
             <div className="text-center md:text-left mb-6 md:mb-0 md:w-3/5 order-last md:order-first justify-items-center">
-                <p className="text-3xl sm:text-3xl md:text-5xl font-medium">I'm Sawani Thiwandika</p>
-                <p className="mt-2 text-lg">From Sri Lanka | Passionate about coding & innovation</p>
+                <p className="justify-center text-3xl md:text-4xl lg:text-5xl font-medium">I'm Sawani Thiwandika</p>
+                <p className="mt-2 text-lg">From Sri Lanka | Coding & Innovation</p>
                 <p className="text-3xl sm:text-2xl md:text-3xl font-madium text-purple-400">Full-Stack developer</p>
-                <div className=" justify-items-center">
-                    {/* Apply the shine effect class here */}
-                    <p  className="mt-3 shine-animation sm:text-2xl md:text-3xl font-medium">
-                        Connecting the dots of Invisible tipsy of Brilliance
+                <div className="justify-items-center">
+
+                    <p  className="text-center mt-3 shine-animation sm:text-2xl md:text-3xl font-medium">
+                        Connecting the dots in the invisible tapestry of brilliance;
                     </p>
                     <a
                         href="#contact"
@@ -31,10 +27,9 @@ export function Intro() {
                 <img
                     src={cover}
                     alt="Sawani Thiwandika"
-                    className="mt-20 w-1/2 object-cover filter contrast-125 brightness-40 saturate-10"
+                    className="mt-20 w-1/2 md:w-full lg:w-1/2 object-cover filter contrast-125 brightness-40 saturate-10"
                 />
             </div>
         </section>
     );
 }
-
